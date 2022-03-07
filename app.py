875d77e4f07d1c8dfe8d9895568bd0bfe93ac54f
Parth Panchal <98511299+parthp-7span@users.noreply.github.com>
2022-03-07 10:38:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,35 +10,40 @@ class Index(Resource):
     def get(self):
         return jsonify(message="captcha ocr demo endpoint...")
 
+
 class Predict(Resource):
     def get(self):
         return jsonify(message="send a post request...")
 
     def post(self):
+        predictor = utils.Predictor(api_key='AIzaSyByI4h7HRINmQOKjl92TgtPm9S672qT_tU',
+                                    url='https://vision.googleapis.com/v1/images:annotate')
         json_data = request.get_json(force=True)
         img_uri = json_data['uri']
-        
-        predictor = utils.Predictor(api_key='AIzaSyByI4h7HRINmQOKjl92TgtPm9S672qT_tU',
-                                url='https://vision.googleapis.com/v1/images:annotate')
-        
         img_numpy = predictor.readb64(img_uri)
 
         response1 = predictor.sendRequest(img_numpy)
         text1 = predictor.responseToText(response1)
         operation = predictor.findOperation(text1)
+        for index, i in enumerate(text1):
+            if i.isnumeric():
+                question = text1[:index]
+                break
 
         processed_img = predictor.getProcessedImg(img_numpy)
         response2 = predictor.sendRequest(processed_img)
         text2 = predictor.responseToText(response2)
         values = predictor.getVals(text2, operation)
 
-        final_output = predictor.findResult(values,operation)
+        question = question + str(values)
 
-        return jsonify(message="success",ocr1=text1, ocr2=text2, result=final_output)
+        final_output = predictor.findResult(values, operation)
+
+        return jsonify(message="success", question=question, result=final_output)
 
 
 api.add_resource(Predict, '/predict')
 api.add_resource(Index, '/')
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     app.run()
